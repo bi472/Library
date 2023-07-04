ae80bcf3fff575d37ed5325232001a546bef0f82
Aidar <matt.nolimiteron@gmail.com>
2023-07-04 15:16:04 +0300

    mongo connect

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const {v4: uuid} = require('uuid');
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 const userRouter = require('./routes/user')
 
+const DATABASE_URL = process.env.mongoURL;
+
 const app = express();
 app.use(express.json())
+
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Ошибка подключения к базе данных:'));
+db.once('open', () => {
+  console.log('Успешное подключение к базе данных!');
+});
 
 app.use('/api/books', booksRouter)
 app.use('/api/user', userRouter)
